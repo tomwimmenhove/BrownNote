128ee336919b66bfb4ec96eafee763002218929b
Tom Wimmenhove <tom.wimmenhove@gmail.com>
2021-10-08 20:41:49 +0100

    Added mixer

--- src/BrownNote.cpp
+++ src/BrownNote.cpp
@@ -14,28 +14,76 @@
 typedef float signalType;
 
 template <typename T>
-class PullDataStream
+class DataStream
 {
 public:
 	virtual size_t PushToVector(std::vector<T>&) = 0;
 
-	virtual ~PullDataStream() { }
+	virtual ~DataStream() { }
 };
 
 template <typename T>
-class DumbSource: public PullDataStream<T>
+class DumbSource: public DataStream<T>
 {
 public:
+	DumbSource(T start = 1)
+		: start(start)
+	{ }
+
 	size_t PushToVector(std::vector<T>& v) override
 	{
-		T x[] = { 1, 2, 3, 4, };
+		T x[] = { start + 0, start + 1, start + 2, start + 3, };
 		size_t n = sizeof(x) / sizeof(T);
 
 		v.reserve(v.size() + n);
-		copy(&x[0], &x[n], back_inserter(v));
+		std::copy(&x[0], &x[n], back_inserter(v));
 
 		return n;
 	}
+
+private:
+	T start;
+};
+
+template <typename T>
+class Mixer: public DataStream<T>
+{
+public:
+	Mixer(std::initializer_list<std::shared_ptr<DataStream<T>>> dataStreams)
+		: dataStreams(dataStreams)
+	{ }
+
+	size_t PushToVector(std::vector<T>& v) override
+	{
+		if (dataStreams.size() == 0)
+		{
+			v.resize(v.size() + 1024);
+		}
+
+		auto start = v.end();
+		size_t len = dataStreams[0]->PushToVector(v);
+
+		for(auto it = dataStreams.begin() + 1; it < dataStreams.end(); it++)
+		{
+			tmpBuf.clear();
+			if ((*it)->PushToVector(tmpBuf) != len)
+			{
+				// Die
+			}
+
+			/* Mix it */
+			for(size_t i = 0; i < tmpBuf.size(); i++)
+			{
+				start[i] += tmpBuf[i];
+			}
+		}
+
+		return len;
+	}
+
+private:
+	std::vector<T> tmpBuf;
+	std::vector<std::shared_ptr<DataStream<T>>> dataStreams;
 };
 
 //template <typename T>
@@ -49,10 +97,10 @@ public:
 //};
 
 template <typename T>
-class DataBuffer : public PullDataStream<T>
+class DataBuffer : public DataStream<T>
 {
 public:
-	DataBuffer(std::shared_ptr<PullDataStream<T>> dataStream, size_t len)
+	DataBuffer(std::shared_ptr<DataStream<T>> dataStream, size_t len)
 	: dataStream(dataStream), len(len)
 	{ }
 
@@ -61,85 +109,55 @@ public:
 		v.reserve(v.size() + len);
 
 		// XXX: Replace this with a circular buffer
-		while (tmpQueue.size() < len)
+		while (tmpBuf.size() < len)
 		{
-			tmpBuf.clear();
 			dataStream->PushToVector(tmpBuf);
-
-			for(auto& x: tmpBuf)
-			{
-				tmpQueue.push(x);
-			}
 		}
 
-		std::vector<T> result(len);
-		for(size_t i = 0; i < len; i++)
-		{
-			v.push_back(tmpQueue.front());
-			tmpQueue.pop();
-		}
+		std::copy(tmpBuf.begin(), tmpBuf.begin() + len, back_inserter(v));
+		tmpBuf.erase(tmpBuf.begin(), tmpBuf.begin() + len);
 
 		return len;
 	}
 
+	inline size_t size() const { return len; }
+
 private:
-	std::shared_ptr<PullDataStream<T>> dataStream;
+	std::shared_ptr<DataStream<T>> dataStream;
 	size_t len;
-	std::queue<T> tmpQueue;
 	std::vector<T> tmpBuf;
 };
 
-
-//template <typename T>
-//class DataStream
-//{
-//	virtual void Push(std::vector<T>&& data) = 0;
-//};
-//
-//template <typename T>
-//class DumbSink : public DataStream<T>
-//{
-//	void Push(std::vector<T>&&) override
-//	{ }
-//};
-//
-//template <typename T>
-//class DumbSource
-//{
-//public:
-//	DumbSource(std::shared_ptr<DataStream<T>> dataStream)
-//	{
-//		this->dataStream = dataStream;
-//	}
-//
-//private:
-//	std::shared_ptr<DataStream<T>> dataStream;
-//};
-
 int main() {
-//	auto sink = std::make_shared<DumbSink<signalType>>();
-//
-//	auto source = std::make_shared<DumbSource<signalType>>(sink);
+	//auto source1 = std::shared_ptr<DataStream<signalType>>(new DumbSource<signalType>());
+	auto source1 = std::make_shared<DumbSource<signalType>>();
+	auto source2 = std::make_shared<DumbSource<signalType>>();
+	auto source3 = std::make_shared<DumbSource<signalType>>(10);
 
-	auto source = std::shared_ptr<PullDataStream<signalType>>(new DumbSource<signalType>());
+	auto buf1 = std::make_shared<DataBuffer<signalType>>(source1, 5);
+	auto buf2 = std::make_shared<DataBuffer<signalType>>(source2, 5);
+	auto buf3 = std::make_shared<DataBuffer<signalType>>(source3, 5);
 
-	auto buf = std::make_shared<DataBuffer<signalType>>(source, 3);
+	auto mix = std::make_shared<Mixer<signalType>>(
+			std::initializer_list<std::shared_ptr<DataStream<signalType>>>({buf1, buf2, buf3}));
 
 	std::vector<signalType> v;
+	v.reserve(buf1->size());
 
-	buf->PushToVector(v);
+	mix->PushToVector(v);
 	for (auto& x: v)
 	{
 		std::cout << x << '\n';
 	}
 
 	v.clear();
-	buf->PushToVector(v);
+	mix->PushToVector(v);
 	for (auto& x: v)
 	{
 		std::cout << x << '\n';
 	}
 
-	std::cout << "Hi" << std::endl; // prints !!!Hello World!!!
+	std::cout << "Done\n";
+
 	return 0;
 }
