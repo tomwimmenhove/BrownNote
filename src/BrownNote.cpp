42ff3c373597f5ca078bbaeeb03b45045f86dd3a
Tom Wimmenhove <tom.wimmenhove@gmail.com>
2021-10-09 13:10:26 +0100

    Optimization. Made it worse..

--- src/BrownNote.cpp
+++ src/BrownNote.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <vector>
 #include <functional>
+#include <algorithm>
 #include <memory>
 #include <queue>
 #include <cmath>
@@ -21,7 +22,7 @@ template <typename T>
 class DataStream
 {
 public:
-	virtual size_t PushToVector(std::vector<T>&) = 0;
+	virtual std::vector<T>& getData() = 0;
 
 	virtual ~DataStream() { }
 };
@@ -31,22 +32,16 @@ class DumbSource: public DataStream<T>
 {
 public:
 	DumbSource(T start = 0)
-		: start(start)
+		: buffer({ start + 0, start + 1, start + 2, start + 3, })
 	{ }
 
-	size_t PushToVector(std::vector<T>& v) override
+	std::vector<T>& getData() override
 	{
-		T x[] = { start + 0, start + 1, start + 2, start + 3, };
-		size_t n = sizeof(x) / sizeof(T);
-
-		v.reserve(v.size() + n);
-		std::copy(&x[0], &x[n], back_inserter(v));
-
-		return n;
+		return buffer;
 	}
 
 private:
-	T start;
+	std::vector<T> buffer;
 };
 
 template <typename T>
@@ -54,24 +49,14 @@ class DcSource: public DataStream<T>
 {
 public:
 	DcSource(T dcValue, size_t n = 1024)
-		: dcValue(dcValue), n(n)
+		: dcValue(dcValue), buffer(n, dcValue)
 	{ }
 
-	size_t PushToVector(std::vector<T>& v) override
-	{
-		v.reserve(v.size() + n);
-
-		for (size_t i = 0; i < n; i++)
-		{
-			v.push_back(dcValue);
-		}
-
-		return n;
-	}
+	std::vector<T>& getData() override { return buffer; }
 
 private:
 	T dcValue;
-	size_t n;
+	std::vector<T> buffer;
 };
 
 template <typename T>
@@ -79,14 +64,14 @@ class SineSource: public DataStream<T>
 {
 public:
 	SineSource(double rate, T amplitude = 1.0, size_t n = 1024)
-		: inc(rate * M_PI * 2), amplitude(amplitude),  n(n), x(0)
+		: inc(rate * M_PI * 2), amplitude(amplitude),  buf(n), x(0)
 	{ }
 
-	size_t PushToVector(std::vector<T>& v) override
+	std::vector<T>& getData() override
 	{
-		for(size_t i = 0; i < n; i++)
+		for(size_t i = 0; i < buf.size(); i++)
 		{
-			v.push_back(sin(x) * amplitude);
+			buf[i] = sin(x) * amplitude;
 			x += inc;
 
 			if (x > M_PI * 2)
@@ -95,13 +80,13 @@ public:
 			}
 		}
 
-		return n;
+		return buf;
 	}
 
 private:
 	double inc;
 	T amplitude;
-	size_t n;
+	std::vector<T> buf;
 	T x;
 };
 
@@ -113,21 +98,20 @@ public:
 	: dataStream(dataStream), gain(gain)
 	{ }
 
-	size_t PushToVector(std::vector<T>& v) override
+	std::vector<T>& getData() override
 	{
-		auto start = v.size();
+		auto& data = dataStream->getData();
 
-		size_t ret = dataStream->PushToVector(v);
+		buf.resize(data.size());
 
-		for(auto i = start; i < v.size(); i++)
-		{
-			v[i] *= gain;
-		}
+		std::transform(data.begin(), data.end(), buf.begin(),
+				std::bind(std::multiplies<T>(), std::placeholders::_1, gain));
 
-		return ret;
+		return buf;
 	}
 
 private:
+	std::vector<T> buf;
 	std::shared_ptr<DataStream<T>> dataStream;
 	T gain;
 };
@@ -140,22 +124,21 @@ public:
 	: dataStream(dataStream), channels(channels), channel(0)
 	{ }
 
-	size_t PushToVector(std::vector<T>& v) override
+	std::vector<T>& getData() override
 	{
 		if (channel == 0)
 		{
 			buf.clear();
-			dataStream->PushToVector(buf);
+			auto& data = dataStream->getData();
+			std::copy(data.begin(), data.end(), back_inserter(buf));
 		}
-
-		std::copy(buf.begin(), buf.end(), std::back_inserter(v));
 
 		if (++channel >= channels)
 		{
 			channel = 0;
 		}
 
-		return buf.size();
+		return buf;
 	}
 
 private:
@@ -166,7 +149,7 @@ private:
 };
 
 template <typename T>
-class Mixer: public DataStream<T>
+class Mixer : public DataStream<T>
 {
 public:
 	Mixer(std::initializer_list<std::shared_ptr<DataStream<T>>> dataStreams)
@@ -179,39 +162,44 @@ public:
 		: dataStreams(dataStreams), combiner(combiner)
 	{ }
 
-	size_t PushToVector(std::vector<T>& v) override
+	std::vector<T>& getData() override
 	{
+		buf.clear();
+
 		if (dataStreams.size() == 0)
 		{
-			v.resize(v.size() + 1024);
+			buf.resize(1024);
+
+			return buf;
 		}
 
-		auto start = v.size();
-		size_t len = dataStreams[0]->PushToVector(v);
+		auto& data0 = dataStreams[0]->getData();
+		std::copy(data0.begin(), data0.end(), back_inserter(buf));
 
 		for(auto it = dataStreams.begin() + 1; it < dataStreams.end(); it++)
 		{
-			tmpBuf.clear();
-			if ((*it)->PushToVector(tmpBuf) != len)
+			auto& data = (*it)->getData();
+			if (data0.size() != data.size())
 			{
-				// Die
+				std::cerr << "Size mismatch!\n";
+				return buf;
 			}
 
 			/* Mix it */
-			for(size_t i = 0; i < tmpBuf.size(); i++)
+			for(size_t i = 0; i < buf.size(); i++)
 			{
-				//v[start + i] += tmpBuf[i];
-				v[start + i] = combiner(v[start + i], tmpBuf[i]);
+				buf[i] = combiner(buf[i], data[i]);
+				//buf[i] += data[i];
 			}
 		}
 
-		return len;
+		return buf;
 	}
 
 	size_t numStreams() const { return dataStreams.size(); }
 
 private:
-	std::vector<T> tmpBuf;
+	std::vector<T> buf;
 	std::vector<std::shared_ptr<DataStream<T>>> dataStreams;
 	std::function<T(T, T)> combiner;
 };
@@ -228,13 +216,12 @@ public:
 	{
 		while(true)
 		{
-			buf.clear();
-			dataStream->PushToVector(buf);
+			auto& data = dataStream->getData();
 
 			audio.clear();
-			audio.reserve(buf.size());
+			audio.reserve(data.size());
 
-			for(auto& sample: buf)
+			for(auto& sample: data)
 			{
 				int x = sample * 32768.0;
 
@@ -250,7 +237,6 @@ public:
 	}
 
 private:
-	std::vector<T> buf;
 	std::vector<int16_t> audio;
 	std::shared_ptr<DataStream<T>> dataStream;
 	Alsa alsa;
@@ -261,31 +247,34 @@ class DataBuffer : public DataStream<T>
 {
 public:
 	DataBuffer(std::shared_ptr<DataStream<T>> dataStream, size_t len)
-	: dataStream(dataStream), len(len)
+	: dataStream(dataStream), buf(len), len(len)
 	{ }
 
-	size_t PushToVector(std::vector<T>& v) override
+	std::vector<T>& getData() override
 	{
-		v.reserve(v.size() + len);
-
 		// XXX: Replace this with a circular buffer
-		while (tmpBuf.size() < len)
+		while (tmpBuf.size() < buf.size())
 		{
-			dataStream->PushToVector(tmpBuf);
+			auto& data = dataStream->getData();
+			tmpBuf.insert(tmpBuf.end(), data.begin(), data.end());
 		}
 
-		std::copy(tmpBuf.begin(), tmpBuf.begin() + len, back_inserter(v));
+		buf.clear();
+		buf.insert(buf.begin(), tmpBuf.begin(), tmpBuf.begin() + len);
+
+		//std::copy(tmpBuf.begin(), tmpBuf.begin() + len, back_inserter(buf));
 		tmpBuf.erase(tmpBuf.begin(), tmpBuf.begin() + len);
 
-		return len;
+		return buf;
 	}
 
-	inline size_t size() const { return len; }
+	inline size_t size() const { return buf.size(); }
 
 private:
 	std::shared_ptr<DataStream<T>> dataStream;
-	size_t len;
+	std::vector<T> buf;
 	std::vector<T> tmpBuf;
+	size_t len;
 };
 
 double fRand(double fMin, double fMax)
@@ -300,43 +289,85 @@ double littleError(double x, double scale = 50.0)
 	return x + fRand(-max, max);
 }
 
-int main()
+std::shared_ptr<DataStream<signalType>> shittyTone(double freq, signalType amplitude,
+		double vibrFreq, signalType vibrAmplitude)
 {
-	auto tone1 = std::make_shared<SineSource<signalType>>(littleError(125.0) / 2 / 48000.0, 0.2);
-	auto tone2 = std::make_shared<SineSource<signalType>>(littleError(125.0) / 4 / 48000.0, 0.3);
-	auto tone3 = std::make_shared<SineSource<signalType>>(littleError(125) / 48000.0, 0.5);
-	auto tone4 = std::make_shared<SineSource<signalType>>(littleError(250) / 48000.0, 1.0);
-	auto tone5 = std::make_shared<SineSource<signalType>>(littleError(500) / 48000.0, 0.5);
-	auto tone6 = std::make_shared<SineSource<signalType>>(littleError(1000.0) / 48000.0, 0.3);
-	auto tone7 = std::make_shared<SineSource<signalType>>(littleError(2000.0) / 48000.0, 0.2);
-	auto tone8 = std::make_shared<SineSource<signalType>>(littleError(4000.0) / 48000.0, 0.05);
-
-	auto tone9 = std::make_shared<SineSource<signalType>>(littleError(2500) * 3 / 48000.0, .05);
-	auto tone10 = std::make_shared<SineSource<signalType>>(littleError(250) / 3 / 48000.0, .15);
-
-	auto combinedTones = std::make_shared<Mixer<signalType>>(
-			std::initializer_list<std::shared_ptr<DataStream<signalType>>>(
-					{tone1, tone2, tone3, tone4, tone5, tone6, tone7, tone8,
-					 tone9, tone10}));
+	auto tone = std::make_shared<SineSource<signalType>>(littleError(freq) / 48000.0, amplitude);
 
 	auto one = std::make_shared<DcSource<signalType>>(1.0);
-	auto vibrato = std::make_shared<SineSource<signalType>>(1.0 / 48000.0, .05);
+	auto vibrato = std::make_shared<SineSource<signalType>>(vibrFreq / 48000.0, vibrAmplitude);
 	auto vibratoScaler = std::make_shared<Mixer<signalType>>(
 			std::initializer_list<std::shared_ptr<DataStream<signalType>>>(
 					{one, vibrato}));
 
-	auto organ = std::make_shared<Mixer<signalType>>(
-			[](const signalType a, const signalType b) { return a * b; },
-			std::initializer_list<std::shared_ptr<DataStream<signalType>>>(
-					{combinedTones, vibratoScaler}));
+	return std::make_shared<Mixer<signalType>>(
+				[](const signalType a, const signalType b) { return a * b; },
+				std::initializer_list<std::shared_ptr<DataStream<signalType>>>(
+						{tone, vibratoScaler}));
+}
+#if 0
+int main()
+{
+	return 0;
+}
+#else
+int main()
+{
+#if 1
+//	auto tone1 = std::make_shared<SineSource<signalType>>(littleError(125.0) / 2 / 48000.0, 0.2);
+//	auto tone2 = std::make_shared<SineSource<signalType>>(littleError(125.0) / 4 / 48000.0, 0.3);
+//	auto tone3 = std::make_shared<SineSource<signalType>>(littleError(125) / 48000.0, 0.5);
+//	auto tone4 = std::make_shared<SineSource<signalType>>(littleError(250) / 48000.0, 1.0);
+//	auto tone5 = std::make_shared<SineSource<signalType>>(littleError(500) / 48000.0, 0.5);
+//	auto tone6 = std::make_shared<SineSource<signalType>>(littleError(1000.0) / 48000.0, 0.3);
+//	auto tone7 = std::make_shared<SineSource<signalType>>(littleError(2000.0) / 48000.0, 0.2);
+//	auto tone8 = std::make_shared<SineSource<signalType>>(littleError(4000.0) / 48000.0, 0.05);
+//
+//	auto tone9 = std::make_shared<SineSource<signalType>>(littleError(2500) * 3 / 48000.0, .05);
+//	auto tone10 = std::make_shared<SineSource<signalType>>(littleError(250) / 3 / 48000.0, .15);
 
-	auto masterChannel = std::make_shared<Gain<signalType>>(organ, 1.0 / combinedTones->numStreams());
+	auto tone1 = shittyTone(125.0 / 2, 0.2, .1, 0.2);
+	auto tone2 = shittyTone(125.0 / 4, 0.3, .3, 0.2);
+	auto tone3 = shittyTone(125, 0.5, .5, 0.2);
+	auto tone4 = shittyTone(250, 1.0, .7, 0.2);
+	auto tone5 = shittyTone(500, 0.5, .6, 0.2);
+	auto tone6 = shittyTone(1000.0, 0.4, .5, 0.2);
+	auto tone7 = shittyTone(2000.0, 0.1, .3, 0.2);
+
+	auto tone9 = shittyTone(2500 * 3, .05, 5, 0.2);
+	auto tone10 = shittyTone(250 / 3, .15, 5, 0.2);
+
+	auto combinedTones = std::make_shared<Mixer<signalType>>(
+			std::initializer_list<std::shared_ptr<DataStream<signalType>>>(
+					{tone1, tone2, tone3, tone4, tone5, tone6, tone7,
+					 tone9, tone10}));
+
+//	auto one = std::make_shared<DcSource<signalType>>(1.0);
+//	auto vibrato = std::make_shared<SineSource<signalType>>(1.0 / 48000.0, .05);
+//	auto vibratoScaler = std::make_shared<Mixer<signalType>>(
+//			std::initializer_list<std::shared_ptr<DataStream<signalType>>>(
+//					{one, vibrato}));
+//
+//	auto organ = std::make_shared<Mixer<signalType>>(
+//			[](const signalType a, const signalType b) { return a * b; },
+//			std::initializer_list<std::shared_ptr<DataStream<signalType>>>(
+//					{combinedTones, vibratoScaler}));
+//
+//	auto masterChannel = std::make_shared<Gain<signalType>>(organ, 1.0 / combinedTones->numStreams());
+
+	auto masterChannel = std::make_shared<Gain<signalType>>(combinedTones, 1.0 / combinedTones->numStreams());
 
 	AlsaMonoSink<signalType> sound(masterChannel);
+//
+//	auto beep = std::make_shared<SineSource<signalType>>(1000.0 / 48000.0, 1.0);
+//	auto g = std::make_shared<Gain<signalType>>(beep, 1.0);
+//	AlsaMonoSink<signalType> sound(g);
 
 	sound.run();
 
 	return 0;
+#endif
+
 	auto source1 = std::make_shared<DumbSource<signalType>>(1);
 	auto source2 = std::make_shared<DumbSource<signalType>>(1);
 	auto source3 = std::make_shared<DumbSource<signalType>>(2);
@@ -350,18 +381,14 @@ int main()
 			std::initializer_list<std::shared_ptr<DataStream<signalType>>>(
 					{split, split, buf2}));
 
-	std::vector<signalType> v;
-	v.reserve(buf1->size());
-
-	mix->PushToVector(v);
-	for (auto& x: v)
+	auto& d1 = mix->getData();
+	for (auto& x: d1)
 	{
 		std::cout << x << '\n';
 	}
 
-	v.clear();
-	mix->PushToVector(v);
-	for (auto& x: v)
+	auto& d2 = mix->getData();
+	for (auto& x: d2)
 	{
 		std::cout << x << '\n';
 	}
@@ -370,3 +397,4 @@ int main()
 
 	return 0;
 }
+#endif
